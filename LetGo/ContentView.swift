23b387e268a0bb75ed0ef354333aca4f0034ec14
EvaM3 <tora.buke@gmail.com>
2025-01-22 21:23:40 +0100

    Initial Commit

---
+++ LetGo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LetGo
+//
+//  Created by Eva Sira Madarasz on 22/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LetGo/LetGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetGoApp.swift
+//  LetGo
+//
+//  Created by Eva Sira Madarasz on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct LetGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
